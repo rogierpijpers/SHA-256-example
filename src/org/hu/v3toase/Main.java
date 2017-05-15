33f4fad6a1bde6472e611bee8477c89f103556d7
Rogier Pijpers <rogier.pijpers@student.hu.nl>
2017-05-15 22:48:00 +0200

    SHA-256 implementation

---
+++ src/org/hu/v3toase/Main.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hu.v3toase;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rogier
+ */
+public class Main {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    
+    public static void main(String[] args) throws NoSuchAlgorithmException{
+        String password = "VeryComplicatedPassword";
+        
+        // generate a salt. this should be stored in the database, belonging to a specific user.
+        String salt = SHA256.createRandomSalt();
+        
+        // hash the password with the salt. This should be stored in the database as the password
+        String hashedPassword = SHA256.hash(password, salt);
+        System.out.println("Salt:\t\t"+hashedPassword);
+        System.out.println("Hashed password:\t"+hashedPassword+"\n");
+        System.out.println("--------------------------------------");
+        
+        // We're going to exit the program by entering the correct password.
+        boolean passwordVerified = false;
+        Scanner scanner = new Scanner(System.in);
+        
+        while(!passwordVerified){
+            // Read a password from the commandline  
+            System.out.println("\nPlease enter password:");
+            String incomingPassword = scanner.nextLine();
+
+            // Hash the incoming password and compare to the stored value
+            String hashedIncomingPassword = SHA256.hash(incomingPassword, salt);
+            passwordVerified = hashedIncomingPassword.equals(hashedPassword);
+            
+            if(!passwordVerified)
+                System.out.println(ANSI_RED + "Password incorrect." + ANSI_RESET);
+        }
+        
+        scanner.close();
+        System.out.println(ANSI_GREEN + "You are authorized!");
+
+    }
+}
---
+++ src/org/hu/v3toase/SHA256.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hu.v3toase;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+import javax.xml.bind.DatatypeConverter;
+
+/**
+ *
+ * @author Rogier
+ */
+public class SHA256 {
+    private static final Random RANDOM = new SecureRandom();
+    
+    public static String createRandomSalt(){
+        byte[] salt = new byte[16];
+        RANDOM.nextBytes(salt);
+        return DatatypeConverter.printBase64Binary(salt);
+    }
+    
+    public static String hash(String plainText, String salt) throws NoSuchAlgorithmException{
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(plainText.getBytes(StandardCharsets.UTF_8));
+        return DatatypeConverter.printBase64Binary(hash);
+    }
+}
